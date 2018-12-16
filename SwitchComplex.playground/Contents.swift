682289e3ae821c100fab1c1f6df49f2276c4289f
Jinsoo Choi <jinsoo@Jinsooui-MacBook-Pro.local>
2018-12-16 20:46:31 +0900

    review complex switch

---
+++ SwitchComplex.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// practice complex switch statement
+var statusCode: Int = 404
+var errorString: String = "The request failed with the error:"
+
+switch statusCode {
+case 100, 101:
+    errorString += " Informational, \(statusCode)."
+    
+case 204:
+    errorString += " Successful but no content, \(statusCode)."
+    
+case 300...307:
+    errorString += " Redirection, \(statusCode)."
+    
+case 400...417:
+    errorString += " Client error, \(statusCode)."
+    
+case 500...505:
+    errorString += " Server error, \(statusCode)."
+    
+case let unknownCode:
+    errorString = "\(unknownCode) is not a known error code."
+}
+
