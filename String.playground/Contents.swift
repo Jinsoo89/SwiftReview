f17779862006691ea3a400bdc2ed2d10d61d8b9b
Jinsoo Choi <hello.python89@gmail.com>
2018-12-21 10:22:57 +0900

    review string

---
+++ String.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// practice string in Swift
+let playground = "Hello, playground"
+
+// mutable string using var
+var mutablePlayground = "Hello, mutable playground"
+mutablePlayground += "!"
+
+for c: Character in mutablePlayground {
+    print("\(c)")
+}
+
+// unicode
