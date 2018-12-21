9752b2201394b62f95f09bc716aba88f10baa6da
Jinsoo Choi <hello.python89@gmail.com>
2018-12-21 11:54:06 +0900

    complete review string

--- String.playground/Contents.swift
+++ String.playground/Contents.swift
@@ -12,3 +12,35 @@ for c: Character in mutablePlayground {
 }
 
 // unicode
+let firstUni = "\u{1F60E}"
+let secondUni = "\u{1F436}"
+
+let catCharacters: [Character] = ["C", "a", "t"]
+let catString = String(catCharacters)
+
+print(catString)
+print(catCharacters)
+
+// string concat
+let s1 = "Hi"
+let s2 = " there"
+var welcome = s1 + s2
+print(welcome)
+
+welcome.append("!")
+print(welcome)
+
+// index
+welcome[welcome.startIndex]
+welcome[welcome.index(before: welcome.endIndex)]
+
+for index in welcome.indices {
+    print("\(welcome[index]) ", terminator: "")
+}
+
+// suffix, prefix
+if welcome.hasPrefix("Hi") {
+    print("Hi")
+} else if welcome.hasSuffix("there") {
+    print("there")
+}
